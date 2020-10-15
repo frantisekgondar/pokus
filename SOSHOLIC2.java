19c5646cfd81d85b9393ee9856974917c4037996
frantisekgondar <43063705+frantisekgondar@users.noreply.github.com>
2020-10-15 10:59:27 +0200

    Add files via upload

---
+++ SOSHOLIC2.java
@@ -0,0 +1,12 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package sample;
+
+public class SOSHOLIC2 {
+    public SOSHOLIC2() {
+    }
+}
+
